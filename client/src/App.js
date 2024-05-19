8e9ea0c18aa8442a3c24d3664761e0c7eec0af07
tienVC <168052032+tienVC@users.noreply.github.com>
2024-05-20 02:21:27 +0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
         <header className="header">
           <div>This is a multicontainer application</div>
           <Link to="/">Home</Link>
-          <Link to="/otherpage">Other pageT</Link>
+          <Link to="/otherpage">Other page</Link>
         </header>
         <div className="main">
           <Route exact path="/" component={MainComponent} />
